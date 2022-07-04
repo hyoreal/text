b7a68497e8cb87286d6d188a69e346aa6776809f
hyojin <hyoreal51@gmail.com>
2022-07-04 15:51:01 +0900

    test

---
+++ main.java
@@ -0,0 +1,9 @@
+public class main {
+    public static void main(String[] args) {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("문자열 ").append("연결");
+        String str = stringBuilder.toString();
+        System.out.println(stringBuilder);
+        System.out.println(str);
+    }
+}
